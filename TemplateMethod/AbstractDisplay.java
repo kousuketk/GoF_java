3c305cf9a29858af222f6d7b77cec4baef4628cf
kousuke takahashi <pamarikomi_t@icloud.com>
2022-03-01 23:31:30 +0900

    created TemplateMethod pattern

---
+++ TemplateMethod/AbstractDisplay.java
@@ -0,0 +1,14 @@
+public abstract class AbstractDisplay {
+  // open, print, closeはサブクラスに実装を任せる抽象メソッド
+  public abstract void open();
+  public abstract void print();
+  public abstract void close();
+
+  public final void display() {
+    open();
+    for(int i = 0; i < 5; i++) {
+      print();
+    }
+    close();
+  }
+}
